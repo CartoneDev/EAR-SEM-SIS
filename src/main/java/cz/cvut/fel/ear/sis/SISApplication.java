0fd616c629c8fff0c3a5837903571a98a832bc5c
Arso <mr.sen4eg@gmail.com>
2023-11-09 18:43:39 +0100

    initial spring config

---
+++ src/main/java/cz/cvut/fel/ear/sis/SISApplication.java
@@ -0,0 +1,11 @@
+package cz.cvut.fel.ear.sis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SISApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SISApplication.class, args);
+    }
+}
